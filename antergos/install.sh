6567cb47d157fa87583ab91253650d8cd7398a6e
aszadzinski <albert.szadzinski@protonmail.com>
2019-07-12 08:36:12 +0200

    added: setup.sh

--- antergos/install.sh
+++
@@ -1,9 +0,0 @@
-# Packages installer & configurator
-
-sudo pacman -Syu --noconfirm
-sudo pacman -S $(cat pkglist.md) --noconfirm
-yay -S $(cat aurlist.md)
-
-
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
