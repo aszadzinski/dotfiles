2ab8940b80f1cc61c336dc84148978ec0350aef4
aszadzinski <albert.szadzinski@protonmail.com>
2019-08-25 12:48:40 +0200

    termux: removed broken packages

--- termux/pkg.sh
+++ termux/pkg.sh
@@ -12,19 +12,19 @@ rm pointless.gpg
 
 #instaling deps
 pkg update
-pkg install termux-api python python-dev p7zip nmap net-tools make man htop gzip grep git curl cmake openssh vim neofetch tar  julia numpy scipy libzmq-dev libcrypt-dev fftw freetype freetype-dev libpng libpng-dev pkg-config ranger tracepath make-dev -y
+pkg install termux-api python  p7zip nmap net-tools make man htop gzip grep git curl cmake openssh vim neofetch tar  julia fftw  libpng  pkg-config ranger tracepath  -y
 
 #python modules *may take a long time (+10min)*
-LDFLAGS="-lzmq" pip3 install jupyter 
-pip3 install hyperdash click virtualenv matplotlib
+#LDFLAGS="-lzmq" pip3 install jupyter 
+pip3 install hyperdash click virtualenv #matplotlib
 
 #jupyter configuration
-jupyter notebook --generate-config
-echo "Jupyter password:"
-jupyter notebook password
+#jupyter notebook --generate-config
+#echo "Jupyter password:"
+#jupyter notebook password
 
 #generating ssh keys
 ssh-keygen -f ~/.ssh/id_rsa -b 4096
 
 echo "DONE"
-echo "clear"  >> .bashrc
+echo "clear"  >> ~/.bashrc
