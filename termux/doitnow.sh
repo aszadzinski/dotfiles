2ffa3b21828e48aad0fe516b4a71edc1f97e04df
aszadzinski <albert.szadzinski@protonmail.com>
2020-07-16 22:29:44 +0200

    new termux setting REBUILD

--- termux/doitnow.sh
+++ termux/doitnow.sh
@@ -1,3 +1,4 @@
+pkg install curl wget -y
 wget https://raw.githubusercontent.com/aszadzinski/dotfiles/master/termux/packages.txt
 wget https://raw.githubusercontent.com/aszadzinski/dotfiles/master/termux/zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/aszadzinski/dotfiles/master/termux/pkg.sh)"
