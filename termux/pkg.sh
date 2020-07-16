8b9c99cd10825f7aca006f3dfb4eccf6b6c6f136
aszadzinski <albert.szadzinski@protonmail.com>
2020-07-16 22:22:45 +0200

    new termux setting REBUILD

--- termux/pkg.sh
+++ termux/pkg.sh
@@ -1,22 +1,28 @@
 #Termux config https://github.com/aszadzinski/dotfiles/termux/pkg.sh
 #run: sh pkg.sh
 
-pkg update
-pkg install gnupg wget -y
+echo "Restoring termux settings..."
+
+echo "- wake lock"
+termux-wake-lock
+echo "- setting up storage"
+termux-setup-storage
+
+pkg update -y 
+pkg install $(cat packages.txt) -y
+
+echo "- installed packages"
 
 #adding pointless repo
-echo "deb https://its-pointless.github.io/files/ termux extras" >> $PREFIX/etc/apt/sources.list
-wget https://its-pointless.github.io/pointless.gpg
-apt-key add pointless.gpg
-rm pointless.gpg
-
-#instaling deps
-pkg update
-pkg install termux-api python  p7zip nmap net-tools make man htop gzip grep git curl cmake openssh vim neofetch tar  julia fftw  libpng  pkg-config ranger tracepath  dnsutils -y
+#echo "deb https://its-pointless.github.io/files/ termux extras" >> $PREFIX/etc/apt/sources.list
+#wget https://its-pointless.github.io/pointless.gpg
+#apt-key add pointless.gpg
+#rm pointless.gpg
+#pkg update
 
 #python modules *may take a long time (+10min)*
 #LDFLAGS="-lzmq" pip3 install jupyter 
-pip3 install hyperdash click virtualenv #matplotlib
+#pip3 install hyperdash click virtualenv #matplotlib
 
 #jupyter configuration
 #jupyter notebook --generate-config
@@ -24,7 +30,12 @@ pip3 install hyperdash click virtualenv #matplotlib
 #jupyter notebook password
 
 #generating ssh keys
+echo "- ssh config"
 ssh-keygen -f ~/.ssh/id_rsa -b 4096
+
+echo "- setting up oh my zsh"
+cp zshrc ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "DONE"
 echo "clear"  >> ~/.bashrc
