f2698fb49eee4a140d5857cd6d6de662074dd7ce
aszadzinski <albert.szadzinski@protonmail.com>
2022-09-21 15:02:01 +0200

    Termux: updated nvim, symlinks

--- termux/.zshrc
+++ termux/.zshrc
@@ -1,9 +1,13 @@
 export ZSH="/data/data/com.termux/files/home/.oh-my-zsh"
 
-ZSH_THEME="arrow" # set by `omz`
+ZSH_THEME="gnzh" # set by `omz`
 plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
+
+alias vim=nvim
+VISUAL=nvim
+EDITOR=nvim
 
 #clear
 #if [ "$TMUX" = "" ]; then tmux; fi
---
+++ termux/nvim/init.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.hidden = true
+vim.o.hlsearch = false
+vim.o.smartindent = true
+vim.o.errorbells = false
+vim.o.scrolloff = 8
+vim.o.incsearch = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+
+vim.g.noswapfiles = true -- :D
+vim.g.nobackup = true -- :D
+vim.g.signcolumn = "yes"
+
+require('packer-plugins')
+require('user.keymaps')
+require('user.nvim-tree')
+require('user.colors')
